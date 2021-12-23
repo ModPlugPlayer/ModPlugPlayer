6cc93ffa1516ab76d34c80fc83c8b40182921dac
Volkan Orhan <volkanorhan@modplugplayer.org>
2021-12-23 15:21:35 +0300

    Licenses have been added

--- src/AppEventFilter.cpp
+++ src/AppEventFilter.cpp
@@ -1,3 +1,14 @@
+/*
+AppEventFilter class definitions of ModPlug Player
+Copyright (C) 2020 Volkan Orhan
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "AppEventFilter.hpp"
 
 AppEventFilter::AppEventFilter(QMainWindow * mainWindow){
@@ -13,4 +24,3 @@ bool AppEventFilter::eventFilter(QObject* object, QEvent* event) {
 	}
 	return false;
   }
-
