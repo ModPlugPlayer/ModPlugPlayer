9ec017461bbddfb17e13fab0c00ddad3d3e06498
Volkan Orhan <contactme@volkanorhan.com>
2025-04-20 08:13:47 +0300

    Fixes memory management in FFTWImpl
    Ensures proper memory allocation and deallocation
    for FFT input and output buffers in FFTWImpl.
    Allocates complex numbers correctly.
    Sets pointers to null after freeing memory to prevent double frees.

--- src/Implementation/FFT/FFTWImpl.cpp
+++ src/Implementation/FFT/FFTWImpl.cpp
@@ -13,7 +13,7 @@ template<> void FFTWImpl<float>::initialize(size_t inputDataElementAmount) {
     }
     size_t outputDataElementAmount = FFTUtil::getOutputDataElementAmount(inputDataElementAmount);
     fftInput = fftwf_alloc_real(inputDataElementAmount);
-    fftOutput = reinterpret_cast<std::complex<float>*>(outputDataElementAmount);
+    fftOutput = reinterpret_cast<std::complex<float>*>(fftwf_alloc_complex(outputDataElementAmount));
     fftPlan = (void *) fftwf_plan_dft_r2c_1d(inputDataElementAmount, fftInput, reinterpret_cast<fftwf_complex*>(&fftOutput[0]), fftPlanType);
     // 2n-1 for example, for 12 fftplan would be 23
 
@@ -47,13 +47,18 @@ template<> void FFTWImpl<double>::execute() {
 
 template<> void FFTWImpl<float>::close() {
     fftwf_destroy_plan((fftwf_plan) fftPlan);
+    fftPlan = nullptr;
     fftwf_free(fftInput);
+    fftInput = nullptr;
     fftwf_free(fftOutput);
+    fftOutput = nullptr;
 }
 
 template<> void FFTWImpl<double>::close() {
-    fftw_destroy_plan((fftw_plan) fftPlan);
-    fftw_free(fftInput);
-    fftw_free(fftOutput);
-    fftw_cleanup();
+    fftwf_destroy_plan((fftwf_plan) fftPlan);
+    fftPlan = nullptr;
+    fftwf_free(fftInput);
+    fftInput = nullptr;
+    fftwf_free(fftOutput);
+    fftOutput = nullptr;
 }
