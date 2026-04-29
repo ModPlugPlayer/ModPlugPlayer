d4e512db2e0f4582b2a75398861ce372c7ab5200
Volkan Orhan <contactme@volkanorhan.com>
2026-04-29 19:57:51 +0300

    Generate dedicated wizard image for Inno Setup
    The installer now uses a custom-generated image for the wizard, ensuring proper scaling and appearance instead of directly using the main installer icon.

--- Installers/InnoSetup/Icons/CreateIcon.sh
+++ Installers/InnoSetup/Icons/CreateIcon.sh
@@ -2,3 +2,9 @@ magick ./Installer_Icon_512x512.png \
   -background none \
   -define icon:auto-resize=256,128,64,48,32,24,16 \
   ModPlugPlayerInstaller.ico
+
+magick ./Installer_Icon_512x512.png \
+  -background none \
+  -gravity center \
+  -extent 560x560 \
+  WizardSmallImageFile.png
